cfa2632a3458ac51fbd7ad6f8f3160e663ae0b10
bnduong <52676812+bnduong@users.noreply.github.com>
2020-04-10 15:28:33 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -2,13 +2,13 @@
     //local development server connection
     $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
     $username = 'root';
-    //$password = 'pa55word';
+    $password = 'Mongmo12';
 
     // Heroku connection
     /*
-    $dsn = 'mysql:host=AVeryLongURLprovidedforJawsDBhost;dbname=YourJawsDBdbname';
-    $username = 'Your JawsDB username';
-    $password = 'Your JawsDB password';
+    $dsn = 'mysql:host=u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=z5m57v1tc8e1ph0q';
+    $username = 'i3t3dhhtdkjk51o7';
+    $password = 'w4s9jn3zukeppir4';
     */
     try {
         //local development server connection
@@ -16,7 +16,7 @@
         $db = new PDO($dsn, $username);
 
         // Heroku connection
-        //$db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
