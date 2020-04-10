92b2c58506a1e214b0fa9bbace55dee370b863fb
bnduong <52676812+bnduong@users.noreply.github.com>
2020-04-10 15:31:42 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,15 +1,11 @@
 <?php
-    //local development server connection
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    $password = 'Mongmo12';
 
     // Heroku connection
-    /*
+    
     $dsn = 'mysql:host=u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=z5m57v1tc8e1ph0q';
     $username = 'i3t3dhhtdkjk51o7';
     $password = 'w4s9jn3zukeppir4';
-    */
+   
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
