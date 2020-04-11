a08720015f75be7fd1b5dcd8c4dc071dca8ee29d
bnduong <52676812+bnduong@users.noreply.github.com>
2020-04-11 11:31:45 -0400

    Update zua-admin.php

--- zua-admin.php
+++ zua-admin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 //require_once('util/secure_conn.php');
-require_once('util/valid_admin.php');
+//require_once('util/valid_admin.php');
 require('model/database.php'); 
 require('model/vehicle_db.php');
 require('model/type_db.php');
